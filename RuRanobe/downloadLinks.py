4bfadcc3599d3516428231efa7cd6be97022a13f
i3sey <deniskedrovsky@mail.ru>
2023-03-11 01:29:13 +0500

    minor changes like pep8-style and delete selenium

--- RuRanobe/downloadLinks.py
+++ RuRanobe/downloadLinks.py
@@ -1,23 +1,16 @@
 from bs4 import BeautifulSoup as bs
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
+
 
 def downloadLinks(link):
-    # driver = webdriver.Chrome()
-    # driver.get(link)
-    # element = driver.find_element(By.XPATH, "/html/body/app-root/app-theme/div[1]/app-layout/main/div/app-project/section/div/div/div[2]/div[2]/div/app-volume-download/button")
-    # ActionChains(driver).click(element).perform()
-    # pass
     r = requests.get(link)
     soup = bs(r.text, "html.parser")
     links = {}
-    new_var = soup.find_all('div', class_ = 'download__buttons')
+    new_var = soup.find_all('div', class_='download__buttons')
     for form in new_var:
         for an in form:
-            links.update({an.text:f"https://ruranobe.ru{an['href']}"})
+            links.update({an.text: f"https://ruranobe.ru{an['href']}"})
     return links
-    
+
+
 print(downloadLinks('https://ruranobe.ru/r/tnynn/v1'))
--- RuRanobe/tests.py
+++ RuRanobe/tests.py
@@ -1,25 +1,38 @@
 import unittest
-import desription, mainPage, names, statuses, tags, volumes, downloadLinks
+import desription
+import mainPage
+import names
+import statuses
+import tags
+import volumes
+import downloadLinks
+
 
 class Tests(unittest.TestCase):
 
     def testMainPage(self):
         self.assertEqual(bool(mainPage.GetListOnMainPage()), True)
-    
+
     def testDesc(self):
-        self.assertEqual(bool(desription.get_desc('https://ruranobe.ru/r/oregairu')), True)
-    
+        self.assertEqual(bool(desription.get_desc(
+            'https://ruranobe.ru/r/oregairu')), True)
+
     def testName(self):
-        self.assertEqual(bool(names.get_names('https://ruranobe.ru/r/oregairu')), True)
-        
+        self.assertEqual(bool(names.get_names(
+            'https://ruranobe.ru/r/oregairu')), True)
+
     def testStatuses(self):
-        self.assertEqual(bool(statuses.get_status('https://ruranobe.ru/r/oregairu')), True)
-        
+        self.assertEqual(bool(statuses.get_status(
+            'https://ruranobe.ru/r/oregairu')), True)
+
     def testTags(self):
-        self.assertEqual(bool(tags.get_tags('https://ruranobe.ru/r/oregairu')), True)
-        
+        self.assertEqual(
+            bool(tags.get_tags('https://ruranobe.ru/r/oregairu')), True)
+
     def testVolumes(self):
-        self.assertEqual(bool(volumes.get_volumes('https://ruranobe.ru/r/oregairu')), True)
-    
+        self.assertEqual(bool(volumes.get_volumes(
+            'https://ruranobe.ru/r/oregairu')), True)
+
     def testDownLinks(self):
-        self.assertEqual(bool(downloadLinks.downloadLinks('https://ruranobe.ru/r/oregairu/v1')), True)
+        self.assertEqual(bool(downloadLinks.downloadLinks(
+            'https://ruranobe.ru/r/oregairu/v1')), True)
